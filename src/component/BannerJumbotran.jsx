55cd93eb9eebff05da1d3dcc6d421120928d1f28
Aswinkumar KC <rama.aswinkumar@gmail.com>
2023-12-24 12:04:10 +0000

    files uploaded

---
+++ src/component/BannerJumbotran.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+function Banner() {
+    return ( <>
+   <div className="container-fluid bg-dark text-white p-5">
+  <div className="jumbotron p-md-5 p-3 ">
+    <h1>Shop in Style</h1>
+    <p>with this shop home page template</p>
+  </div>
+
+</div>
+</> );
+}
+
+export default Banner;
